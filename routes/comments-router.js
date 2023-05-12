df6ae1f5689fd610b614b72d6cb9eb26d31f8f69
Saima Khanom <saimakhanom@gmail.com>
2023-05-12 14:22:46 +0100

    add router and isolate error handlers

---
+++ routes/comments-router.js
@@ -0,0 +1,8 @@
+const { deleteComment } = require("../controllers/comments.controller");
+
+const commentsRouter = require("express").Router();
+
+commentsRouter.delete("/:comment_id", deleteComment);
+
+
+module.exports = commentsRouter;
