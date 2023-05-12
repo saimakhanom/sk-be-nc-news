6b7eb89e40736091127fd29bd13ba770b579c880
Saima Khanom <saimakhanom@gmail.com>
2023-05-12 12:01:36 +0100

    host db

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -3,6 +3,7 @@ const seed = require('./seed.js');
 const db = require('../connection.js');
 
 const runSeed = () => {
+  console.log(`Database ${process.env.PGDATABASE || process.env.DATABASE_URL} seeded.`)
   return seed(devData).then(() => db.end());
 };
 
